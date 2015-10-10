2aa6dec4dcb95108661b76b5dd5228c4487f05cf
Khalid <khalid@Londoner>
2015-10-10 18:53:12 -0400

    set up server to the game

--- app.js
+++ app.js
@@ -2,5 +2,11 @@
 var express = require('express');
 var app = express();
 var server = require('http').Server(app);
-
+var port = process.env.port || 3000;
+// routes
+app.get('/', function(req, res) {
+	res.sendFile(__dirname + 'index.html');
+});
+// middleware
+app.use('static', express.static('static'));
 
