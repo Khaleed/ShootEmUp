f10c54643183a9f1f7431e03f54aa0071e51eccf
Khalid <khalid@Londoner>
2015-10-10 18:38:30 -0400

    add files

---
+++ app.js
@@ -0,0 +1,6 @@
+// set up a server for the game
+var express = require('express');
+var app = express();
+var server = require('http').Server(app);
+
+
---
+++ game.js
@@ -0,0 +1,28 @@
+// Game Constructor
+function Game() {
+	var screen = document.getElementById('screen').getContext('2d');
+	var self = this;
+	// spaceraiders is real-time and therefore we need something
+	// to deal with the animation
+	function tick() {
+		// update the current game state
+		self.update();
+		// draw the current game state
+		self.draw(screen);
+	}
+	tick();
+};
+
+Game.prototype =  {
+	update: function() {
+
+	},
+	draw: function(screen) {
+		console.log('hey there');
+	}
+
+}; 
+
+window.addEventListener('load', function() {
+	new Game();
+});
