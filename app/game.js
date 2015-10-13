5d1db56dfa625eed4cb77f5c1a9b8d2c8b0ed9a5
Khalid <khalid@Londoner>
2015-10-13 11:11:00 -0400

    incorporate webpack and babel.js

--- app/game.js
+++ app/game.js
@@ -1,3 +1,4 @@
+// assign
 var canvas = document.getElementById('screen');
 var ctx = canvas.getContext('2d');
 
@@ -6,42 +7,59 @@ canvas.height = 600;
 
 var x = 100;
 var y = 100;
+
 // instantiate player obj
-var player = new Rectangle(100,100, 300, 300); 
-// add listeners to know when they press keys and let them go
-document.addEventListener('keyup', function(e) {
-	console.log('key up event working');
-	if (e.keyCode === 37) {
-		player.x-=19;
-	}
-});
-document.addEventListener('keydown', function(e) {
-	
-});
-
-
+var player = new Enemy(canvas.width / 2, canvas.height - 50, 32, 32);
+player.color = "blue";
+// instantiate invader obj
+var invader = new Enemy();
 // update the player's game logic on every key pressed
 player.update = function() {
 
 };
+// add listeners for when they press the key
+document.addEventListener('keyup', function(e) {
+	console.log('key up event working');
+	if (e.keyCode === 37) {
+		// move player
+		//player.x-=8;
+	}
+});
+
+// when they push DOWN
+document.addEventListener('keydown', function(e) {
+	if (e.keyCode === 37) {
+		// player.x-=8;
+	}
+});
+
 // animate
-function update(){
+function update() {
 	// clear screen to get ready for next frame
 	ctx.clearRect(0, 0, 400, 600);
 	drawRect(player);
+	//player.update();
 	//player.y+=1;
 	setTimeout(update, 30);
-} 
+}
 
 update();
+
 // make Rectangle class
-function Rectangle(x,y, w, h) {
+function Rectangle(x, y, w, h) {
 	this.x = x;
 	this.y = y;
 	this.w = w;
 	this.h = h;
 	this.color = 'white';
 }
+// Enemy inherits from Rectangle
+function Enemy(x, y) {
+	// inherit from Rectangle
+	Rectangle.call(this, x, y, 35, 35);
+	this.color = 'red';
+}
+
 // draw any rectangle
 function drawRect(rect) {
 	// set color of rect
@@ -49,7 +67,3 @@ function drawRect(rect) {
 	// draw rect
 	ctx.fillRect(rect.x, rect.y, rect.w, rect.h);
 }
-
-
-
-
