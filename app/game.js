54bc6a6b45b9fdb32abd5ce19ed7289605658c28
Khaleed <khalid.ali@consultant.com>
2015-10-19 17:27:32 -0400

    add win or lose feature

--- app/game.js
+++ app/game.js
@@ -1,166 +1,173 @@
 'use strict';
-// grab canvas/screen
-var canvas = document.getElementById('screen');
-// fallback if canvas is not supported
-if (canvas.getContext === undefined) {
-	console.error('browser does not support canvas');
-} else {
-	var ctx = canvas.getContext('2d');
-}
-// set properties of canvas
-canvas.width = 800;
-canvas.height = 600;
-// set initial coordinates of canvas
-var x = 0;
-var y = 0;
-var telePortBorder = 500;
-var velX = 2;
-var bulletVel = 10;
-var playerVel = 5;
-// if keys are pressed
-var rightPressedKey = false;
-var leftPressedKey = false;
-// add listeners for when they let the key go
-document.addEventListener('keyup', function(e) {
-	if (e.keyCode === 37) {
-		leftPressedKey = false;
-	} else if (e.keyCode === 39) {
-		rightPressedKey = false;
-	}
-});
-// when they push down
-document.addEventListener('keydown', function(e) {
-	if (e.keyCode === 37) {
-		leftPressedKey = true;
-	} else if (e.keyCode === 39) {
-		rightPressedKey = true;
-	}
-	// condition for shooting
-	else if (e.keyCode === 32) {
-		playerBullets.push(new Bullet(player.x + player.w / 2, player.y));
-	}
-});
-// animation loop because space invaders is a real-time game
-function update() {
-	// clear rect on every frame
-	ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-	player.update();
-	drawRect(player);
-	var leftMostEnemPix = enemies[0].x;
-	var rightMostEnemPix = enemies[enemies.length - 1].x + enemies[0].w;
-
-	if (leftMostEnemPix < 0 || rightMostEnemPix > canvas.width) {
-		console.log('hi');
-		velX *= -1;
-		enemies.forEach(function(item) {
-			item.y += 35;
-		});
+window.addEventListener('load', function() {
+	// grab canvas/screen
+	var canvas = document.getElementById('screen');
+	var status = document.getElementById('status');
+	// fallback if canvas is not supported
+	if (canvas.getContext === undefined) {
+		console.error('browser does not support canvas');
+	} else {
+		var ctx = canvas.getContext('2d');
 	}
-	// this causes playerBullets to move upwards
-	playerBullets.forEach(function(item, indx, array) {
-		console.log(item);
-		item.update();
-		drawRect(item);
+	// set properties of canvas
+	canvas.width = 800;
+	canvas.height = 600;
+	// set initial coordinates of canvas
+	var x = 0;
+	var y = 0;
+	var telePortBorder = 500;
+	var velX = 2;
+	var bulletVel = 10;
+	var playerVel = 5;
+	var gameRunning = true;
+	// if keys are pressed
+	var rightPressedKey = false;
+	var leftPressedKey = false;
+	// add listeners for when they let the key go
+	document.addEventListener('keyup', function(e) {
+		if (e.keyCode === 37) {
+			leftPressedKey = false;
+		} else if (e.keyCode === 39) {
+			rightPressedKey = false;
+		}
 	});
-
-	enemies.forEach(function(item, indx, array) {
-		console.log(item);
-		item.update();
-		drawRect(item);
+	// when they push down
+	document.addEventListener('keydown', function(e) {
+		if (e.keyCode === 37) {
+			leftPressedKey = true;
+		} else if (e.keyCode === 39) {
+			rightPressedKey = true;
+		}
+		// condition for shooting
+		else if (e.keyCode === 32) {
+			playerBullets.push(new Bullet(player.x + player.w / 2, player.y));
+		}
 	});
-	if (leftPressedKey === true) {
-		if (player.x > 0) {
-			player.x -= playerVel;
+	// animation loop because space invaders is a real-time game
+	function update() {
+		// clear rect on every frame
+		ctx.clearRect(0, 0, canvas.width, canvas.height);
+		if (gameRunning) {
+			player.update();
+			drawRect(player);
+			var leftMostEnemPix = enemies[0].x;
+			var rightMostEnemPix = enemies[enemies.length - 1].x + enemies[0].w;
+
+			if (leftMostEnemPix < 0 || rightMostEnemPix > canvas.width) {
+				velX *= -1;
+				enemies.forEach(function(item) {
+					item.y += 35;
+				});
+			}
+			// this causes playerBullets to move upwards
+			playerBullets.forEach(function(item, indx, array) {
+				item.update();
+				drawRect(item);
+			});
+
+			enemies.forEach(function(item, indx, array) {
+				item.update();
+				drawRect(item);
+			});
+			if (leftPressedKey === true) {
+				if (player.x > 0) {
+					player.x -= playerVel;
+				}
+			}
+			if (rightPressedKey === true) {
+				if (player.x < canvas.width - 32) {
+					player.x += playerVel;
+				}
+			}
+			// detect collision
+			bulletEnemyCollision();
+		}
+		setTimeout(update, 1);
+	}
+	// make a Rectangle classnpm install -g node-inspector
+	function Rectangle(x, y, w, h) {
+		this.x = x;
+		this.y = y;
+		this.w = w;
+		this.h = h;
+		this.color = 'white';
+		this.update = function() {
+
+		};
+	}
+
+	// make a Player class that inherits from Rectangle
+	function Player(x, y) {
+		// inherit from Rectangle
+		Rectangle.call(this, canvas.width / 2, canvas.height - 50, 32, 32);
+		this.color = 'blue';
+	}
+	// Enemy inherits from Rectangle
+	function Enemy(x, y) {
+		// inherit from Rectangle
+		Rectangle.call(this, x, y, 25, 25);
+		this.color = 'red';
+		// initially go right
+		this.update = function() {
+			// make enemy move
+			this.x += velX;
+			// set enemies back to top    
+			if (this.y > telePortBorder) {
+				gameRunning = false;
+				status.innerHTML = 'You lose';
+			}
+		};
+	}
+	// create Bullet class
+	function Bullet(x, y) {
+		Rectangle.call(this, x, y, 5, 15);
+		this.color = 'white';
+		this.update = function() {
+			// fire bullet
+			this.y -= bulletVel;
+			// set enemies back to top
 		}
 	}
-	if (rightPressedKey === true) {
-		if (player.x < canvas.width - 32) {
-			player.x += playerVel;
+	// logic for creating enemies
+	var enemies = [];
+	for (var i = 0; i < 8; i += 1) { // this loop controls width of block of enemies
+		for (var j = 0; j < 8; j += 1) { // this loop controls height of block of enemies
+			// space out enemies
+			enemies.push(new Enemy(45 * i, 20 + 45 * j));
 		}
 	}
-	// detect collision
-	bulletEnemyCollision();
-	setTimeout(update, 1);
-}
-// make a Rectangle classnpm install -g node-inspector
-function Rectangle(x, y, w, h) {
-	this.x = x;
-	this.y = y;
-	this.w = w;
-	this.h = h;
-	this.color = 'white';
-	this.update = function() {
-
-	};
-}
-
-// make a Player class that inherits from Rectangle
-function Player(x, y) {
-	// inherit from Rectangle
-	Rectangle.call(this, canvas.width / 2, canvas.height - 50, 32, 32);
-	this.color = 'blue';
-}
-// Enemy inherits from Rectangle
-function Enemy(x, y) {
-	// inherit from Rectangle
-	Rectangle.call(this, x, y, 35, 35);
-	this.color = 'red';
-	// initially go right
-	this.update = function() {
-		// make enemy move
-		this.x += velX;
-		// set enemies back to top    
-		if (this.y > telePortBorder) {
-			this.y -= 500;
-		}
-	};
-}
-// create Bullet class
-function Bullet(x, y) {
-	Rectangle.call(this, x, y, 5, 15);
-	this.color = 'white';
-	this.update = function() {
-		// fire bullet
-		this.y -= bulletVel;
-		// set enemies back to top
+	var playerBullets = [];
+	var player = new Player(32, 32);
+	// draw any rectangle
+	function drawRect(rect) {
+		// set color of rect
+		ctx.fillStyle = rect.color;
+		// draw rect
+		ctx.fillRect(rect.x, rect.y, rect.w, rect.h);
 	}
-}
-// logic for creating enemies
-var enemies = [];
-for (var i = 0; i < 4; i += 1) { // this loop controls width of block of enemies
-	for (var j = 0; j < 4; j += 1) { // this loop controls height of block of enemies
-		// space out enemies
-		enemies.push(new Enemy(45 * i, 20 + 45 * j));
-	}
-}
-var playerBullets = [];
-var player = new Player(32, 32);
-// draw any rectangle
-function drawRect(rect) {
-	// set color of rect
-	ctx.fillStyle = rect.color;
-	// draw rect
-	ctx.fillRect(rect.x, rect.y, rect.w, rect.h);
-}
 
-function bulletEnemyCollision() {
-	for (var i = 0; i < playerBullets.length; i += 1) {
-		for (var j = 0; j < enemies.length; j += 1) {
-			// this is for when playerBullets and enemies collide
-			var c1 = enemies[j].x < playerBullets[i].x + playerBullets[i].w; // right edge of bullet is to the right of left edge of enemy
-			var c2 = playerBullets[i].x < enemies[j].x + enemies[j].w; // left edge of bullet is to the left of right edge of enemy
-			var c3 = enemies[j].y + enemies[j].h > playerBullets[i].y; // top edge of bullet is above bottom edge of enemy
-			var c4 = playerBullets[i].y + playerBullets[i].h > enemies[j].y // if the bottom edge of the bullet is below the top edge of the enemy
-				// collision has happened
-			if (c1 && c2 && c3 && c4) {
-				// remove an invader
-				enemies.splice(j, 1);
-				playerBullets.splice(i, 1);
-				break;
+	function bulletEnemyCollision() {
+		for (var i = 0; i < playerBullets.length; i += 1) {
+			for (var j = 0; j < enemies.length; j += 1) {
+				// this is for when playerBullets and enemies collide
+				var c1 = enemies[j].x < playerBullets[i].x + playerBullets[i].w; // right edge of bullet is to the right of left edge of enemy
+				var c2 = playerBullets[i].x < enemies[j].x + enemies[j].w; // left edge of bullet is to the left of right edge of enemy
+				var c3 = enemies[j].y + enemies[j].h > playerBullets[i].y; // top edge of bullet is above bottom edge of enemy
+				var c4 = playerBullets[i].y + playerBullets[i].h > enemies[j].y // if the bottom edge of the bullet is below the top edge of the enemy
+					// collision has happened
+				if (c1 && c2 && c3 && c4) {
+					// remove an invader
+					enemies.splice(j, 1);
+					playerBullets.splice(i, 1);
+					if (enemies.length === 0) {
+						gameRunning = false;
+						status.innerHTML = 'You win';
+					}
+					break;
+				}
 			}
 		}
 	}
-}
-
-update();
+	update();
+});
