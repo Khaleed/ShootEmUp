f0f02b6142eaaa7eaf34db4529a1a31f009071c4
Khalid Omar Ali <khalid.ali@consultant.com>
2016-01-15 03:42:39 +0000

    jazz it up  and add plugins

--- webpack.config.js
+++ webpack.config.js
@@ -1,37 +1,61 @@
 var webpack = require('webpack');
-
 var path = require('path');
+var ExtractTextPlugin = require("extract-text-webpack-plugin");
 
 module.exports = {
+    // console erros map to correct file and line number
     devtool: 'source-map',
+
     entry: {
-        app: ['./app/game.js'
+        // hot style updates
+        'webpack/hot/dev-server',
+        // refresh on none hot style updates
+        'webpack-dev-server/client?http://localhost:8080',
+        // current app
+        path.resolve(__dirname, 'app', 'game.js')
     },
+
     devServer: {
         contentBase: "public/"
     },
+
     output: {
         path: path.resolve(__dirname, 'public'),
         publicPath: '/'
         filename: 'bundle.js'
     },
+
+    // define module loaders
     module: {
         loaders: [
-
+            // ES6 loader
             {
                 test: /\.jsx?$/,
                 exclude: /(node_modules|bower_components)/,
                 loader: 'babel?optional[]=runtime'
             },
-
+            // JSON loader
             {
                 test: /\.json$/,
                 exclude: /(node_modules|bower_components)/,
                 loader: 'json'
-            }
+            },
+            // CSS Loader
+            {
+                test: /\.css$/,
+                loader: ExtractTextPlugin.extract("style-loader", "css-loader")
+            },
         ]
     },
+
     resolve: {
         extensions: ['.js', '.json', '']
-    }
-}
+    },
+
+    plugins: [
+        // Extract CSS files
+        new ExtractTextPlugin("style.css"),
+        // Hot module replacement
+        new webpack.HotModuleReplacementPlugin()
+    ]
+};
