0e59e88891b5b4d9e691cfa76ba4c366b5d80f32
Khalid Omar Ali <khalid.ali@consultant.com>
2016-01-16 03:36:17 +0000

    add new production config

--- webpack.config.production.js
+++ webpack.config.production.js
@@ -23,7 +23,7 @@ module.exports = {
     },
 
     output: {
-        path: path.resolve(__dirname, 'public'),
+        path: __dirname, 'public',
         publicPath: '/',
         filename: 'bundle.js'
     },
