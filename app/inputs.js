1a133f64531ad6865ed1d876c68304c8830b2a21
Khalid Omar Ali <khalid.ali@consultant.com>
2016-01-19 03:00:58 +0000

    go back

--- app/inputs.js
+++
@@ -1,8 +0,0 @@
-export default {
-	canvas: document.getElementById('screen'),
-	status: document.getElementById('status'),
-	playerShootSound: document.getElementById('player-shoots'),
-	invaderShootSound: document.getElementById('invader-shoots'),
-	invaderDiesSound: document.getElementById('invader-explodes'),
-	playerDiesSound: document.getElementById('player-explodes')
-}
