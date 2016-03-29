3980d7bb8c38980f4d15d6c4579c7ebfc841b521
Khalid Omar Ali <khalid.ali@consultant.com>
2016-03-29 18:36:24 +0100

    remove experimental code

--- app/model.js
+++ app/model.js
@@ -110,28 +110,4 @@ export function EnemyBullet(args) {
 }
 
 
-function range(end, start) {
-	var result = [];
-	for (var i = start; i <= end; i += 1) {
-		result.push(i);
-	}
-}
-
-var numbers = range(1, 100);
-
-numbers.map( c => {
-	if (c % 3 === 0) {
-		console.log("fizz");
-	} else if (c % 5 === 0) {
-		console.log("buzz");
-	} else if (c % 5 === 0 && c % 3 === 0) {
-		console.log("fizzbuzz");
-	} else {
-		console.log(c);
-	}
-});
-
-
-
-
 
