c4b66c676572e2267aa15bb97f26c782864527aa
Khalid Omar Ali <khalid.ali@consultant.com>
2016-09-24 00:36:49 +0100

    remove immediately invoked function expression

--- app/game.js
+++ app/game.js
@@ -4,64 +4,62 @@ import inputs from './input';
 import keys from './keystate';
 import GameState from './state';
 
-(function() {
-    const canvas = inputs.canvas;
-    keys.addListeners();
+const canvas = inputs.canvas;
+keys.addListeners();
 
-    function playSounds(oldState, newState) {
-        if (oldState.enemies.length > newState.enemies.length) {
-            inputs.invaderDiesSound.play();
-        }
+function playSounds(oldState, newState) {
+    if (oldState.enemies.length > newState.enemies.length) {
+        inputs.invaderDiesSound.play();
+    }
+}
+
+window.addEventListener('load', () => {
+    let ctx;
+    const shipImg = document.getElementById('player');
+    const invaderImg = document.getElementById('invader');
+
+    if (canvas.getContext === undefined) {
+        console.error('browser does not support canvas');
+    } else {
+        ctx = canvas.getContext('2d');
+    }
+    canvas.width = 800;
+    canvas.height = 600;
+
+    function drawRect(rect) {
+        ctx.fillStyle = rect.color;
+        ctx.fillRect(rect.x, rect.y, rect.w, rect.h);
     }
 
-    window.addEventListener('load', () => {
-        let ctx;
-        const shipImg = document.getElementById('player');
-        const invaderImg = document.getElementById('invader');
+    function drawPlayer(player) {
+        ctx.drawImage(shipImg, player.x, player.y);
+    }
 
-        if (canvas.getContext === undefined) {
-            console.error('browser does not support canvas');
-        } else {
-            ctx = canvas.getContext('2d');
+    function drawInvader(pos) {
+        ctx.drawImage(invaderImg, pos.x, pos.y);
+    }
+
+    function draw(gameState) {
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        gameState.enemies.map(enemy => drawInvader(enemy));
+        gameState.enemyBullets.concat(gameState.playerBullets).map(bullet => drawRect(bullet));
+        gameState.particles.map(particle => drawRect(particle));
+        if (gameState.player) {
+            drawPlayer(gameState.player);
         }
-        canvas.width = 800;
-        canvas.height = 600;
+        setTimeout(() => update(gameState), 1);
+    }
 
-        function drawRect(rect) {
-            ctx.fillStyle = rect.color;
-            ctx.fillRect(rect.x, rect.y, rect.w, rect.h);
-        }
+    function update(gameState) {
+        const frozenKeys = Object.assign({}, keys);
+        Object.freeze(frozenKeys);
+        const newGameState = gameState.updateIfGameIsRunning(frozenKeys);
+        playSounds(gameState, newGameState);
+        draw(newGameState);
+    }
 
-        function drawPlayer(player) {
-            ctx.drawImage(shipImg, player.x, player.y);
-        }
+    draw(GameState({
+        inputs
+    }));
 
-        function drawInvader(pos) {
-            ctx.drawImage(invaderImg, pos.x, pos.y);
-        }
-
-        function draw(gameState) {
-            ctx.clearRect(0, 0, canvas.width, canvas.height);
-            gameState.enemies.map(enemy => drawInvader(enemy));
-            gameState.enemyBullets.concat(gameState.playerBullets).map(bullet => drawRect(bullet));
-            gameState.particles.map(particle => drawRect(particle));
-            if (gameState.player) {
-                drawPlayer(gameState.player);
-            }
-            setTimeout(() => update(gameState), 1);
-        }
-
-        function update(gameState) {
-            const frozenKeys = Object.assign({}, keys);
-            Object.freeze(frozenKeys);
-            const newGameState = gameState.updateIfGameIsRunning(frozenKeys);
-            playSounds(gameState, newGameState);
-            draw(newGameState);
-        }
-
-        draw(GameState({
-            inputs
-        }));
-
-    });
-}());
+});
