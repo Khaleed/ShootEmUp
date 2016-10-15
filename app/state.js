95ed181404b869752c122b103b9bc9df7b1202bb
Khalid Omar Ali <khalid.ali@consultant.com>
2016-10-15 23:39:48 +0100

    add updatePlayerAction to the that immutable object

--- app/state.js
+++ app/state.js
@@ -281,6 +281,7 @@ export default function GameState(args) {
         playerFinalBulletNframeCount,
         playerBulletNframeCounter,
         player,
+        updatePlayerAction,
         updateIfGameIsRunning,
         bulletCollision,
         enemyCollisionWithBorder,
