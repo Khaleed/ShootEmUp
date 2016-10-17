356384c36276949f8e2762000fced1be502d159f
Khalid Omar Ali <khalid.ali@consultant.com>
2016-10-17 02:37:08 +0100

    stop continuous sound when player dies

--- app/game.js
+++ app/game.js
@@ -12,8 +12,6 @@ import GameState from './state';
     function playSounds(oldState, newState) {
         if (oldState.enemies.length > newState.enemies.length) {
             inputs.invaderDiesSound.play();
-        } else if (Object.keys(newState.player).length === 0) {
-            inputs.playerDiesSound.play();
         }
     }
 
