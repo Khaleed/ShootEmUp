168a336460893b3a465cba9170263da3013d0815
Khalid Omar Ali <khalid.ali@consultant.com>
2016-12-15 23:01:25 +0000

    remove immediately invoked function expression

--- app/game.js
+++ app/game.js
@@ -4,63 +4,60 @@ import inputs from './input';
 import keys from './keystate';
 import GameState from './state';
 
-(function () {
+keys.addListeners();
 
+function playSounds(oldState, newState) {
+    if (oldState.enemies.length > newState.enemies.length) {
+        inputs.invaderDiesSound.play();
+    }
+}
+
+window.addEventListener('load', () => {
     const canvas = inputs.canvas;
-    keys.addListeners();
+    let ctx;
+    const shipImg = document.getElementById('player');
+    const invaderImg = document.getElementById('invader');
 
-    function playSounds(oldState, newState) {
-        if (oldState.enemies.length > newState.enemies.length) {
-            inputs.invaderDiesSound.play();
-        }
+    if (canvas.getContext === undefined) {
+        console.error('browser does not support canvas');
+    } else {
+        ctx = canvas.getContext('2d');
+    }
+    canvas.width = 800;
+    canvas.height = 600;
+
+    function drawRect(rect) {
+        ctx.fillStyle = rect.color;
+        ctx.fillRect(rect.x, rect.y, rect.w, rect.h);
     }
 
-    window.addEventListener('load', () => {
-        let ctx;
-        const shipImg = document.getElementById('player');
-        const invaderImg = document.getElementById('invader');
+    function drawPlayer(player) {
+        ctx.drawImage(shipImg, player.x, player.y);
+    }
 
-        if (canvas.getContext === undefined) {
-            console.error('browser does not support canvas');
-        } else {
-            ctx = canvas.getContext('2d');
-        }
-        canvas.width = 800;
-        canvas.height = 600;
+    function drawInvader(pos) {
+        ctx.drawImage(invaderImg, pos.x, pos.y);
+    }
 
-        function drawRect(rect) {
-            ctx.fillStyle = rect.color;
-            ctx.fillRect(rect.x, rect.y, rect.w, rect.h);
+    function draw(gameState, lastFrameTime) {
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        gameState.enemies.map(enemy => drawInvader(enemy));
+        gameState.enemyBullets.concat(gameState.playerBullets).map(bullet => drawRect(bullet));
+        gameState.particles.map(particle => drawRect(particle));
+        if (gameState.player && !gameState.playerDying) {
+            drawPlayer(gameState.player);
         }
+        requestAnimationFrame(() => update(gameState, lastFrameTime));
+    }
 
-        function drawPlayer(player) {
-            ctx.drawImage(shipImg, player.x, player.y);
-        }
-
-        function drawInvader(pos) {
-            ctx.drawImage(invaderImg, pos.x, pos.y);
-        }
-
-        function draw(gameState, lastFrameTime) {
-            ctx.clearRect(0, 0, canvas.width, canvas.height);
-            gameState.enemies.map(enemy => drawInvader(enemy));
-            gameState.enemyBullets.concat(gameState.playerBullets).map(bullet => drawRect(bullet));
-            gameState.particles.map(particle => drawRect(particle));
-            if (gameState.player && !gameState.playerDying) {
-                drawPlayer(gameState.player);
-            }
-            requestAnimationFrame(() => update(gameState, lastFrameTime));
-        }
-
-        function update(gameState, lastFrameTime) {
-            const thisFrameTime = new Date().getTime();
-            const thisFrameDuration = thisFrameTime - (lastFrameTime || thisFrameTime);
-            const frozenKeys = Object.assign({}, keys);
-            Object.freeze(frozenKeys);
-            const newGameState = gameState.updateIfGameIsRunning(frozenKeys, thisFrameDuration);
-            playSounds(gameState, newGameState);
-            draw(newGameState, thisFrameTime);
-        }
-        draw(GameState({inputs}), 0);
-    });
-}());
+    function update(gameState, lastFrameTime) {
+        const thisFrameTime = new Date().getTime();
+        const thisFrameDuration = thisFrameTime - (lastFrameTime || thisFrameTime);
+        const frozenKeys = Object.assign({}, keys);
+        Object.freeze(frozenKeys);
+        const newGameState = gameState.updateIfGameIsRunning(frozenKeys, thisFrameDuration);
+        playSounds(gameState, newGameState);
+        draw(newGameState, thisFrameTime);
+    }
+    draw(GameState({inputs}), 0);
+});
